6812fa851c6f758a558e1b1f53889e68cb4cf9a6
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-07-11 10:26:56 -0400

    initial commit frontend

--- app.js
+++ app.js
@@ -23,3 +23,33 @@ db.sequelize.sync({ force: true }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+
+// const express = require("express");
+// const handlebars = require("express-handlebars");
+// const app = express();
+// const path = require("path");
+// const port = process.env.PORT || 8080;
+
+
+// app.set("view engine", "handlebars");
+
+// app.engine("handlebars", handlebars({
+//     extname: "handlebars",
+//    layoutsDir: path.join(__dirname + '/views/layouts')
+// }));
+
+
+// app.use(express.static(path.join (__dirname, "public")));
+
+// app.get("/", (req,res)=>{
+//     res.render('index', {layout: 'main'});
+// });
+// app.get("/post", (req,res)=>{
+//     res.render('post');
+// });
+// app.get("/board", (req,res)=>{
+//     res.render('board');
+// })
+
+// app.listen(port, () => console.log(`App listenining on port ${port}`));
