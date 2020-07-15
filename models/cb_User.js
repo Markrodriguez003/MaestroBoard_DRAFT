a7100d3a622c10b1e599e461ef37e6c57efc703b
Mark Rodriguez <Markrodriguez003@gmail.com>
2020-07-15 18:18:24 -0400

    Finalized model FK associations

--- models/cb_User.js
+++ models/cb_User.js
@@ -2,24 +2,24 @@ module.exports = (sequelize, DataTypes) => {
     const cb_User = sequelize.define("cb_User", {
         userName: {
             type: DataTypes.STRING,
+            // unique: true
         },
         userEmail: {
             type: DataTypes.STRING,
+            // unique: true
         },
         userNumber: {
             type: DataTypes.STRING,
+            // unique: true
         },
     });
 
-    // cb_User.associate = function (models) {
-    //     cb_User.hasMany(models.cb_Post, {
-    //         onDelete: "cascade"
-        
-    //         // foreignKey: {
-    //         //     allowNull: false
-    //         // }
-    //     });
-    // };
+    cb_User.associate = models => {
+        cb_User.hasMany(models.cb_Post, {
+            foreignKey: 'fk_user',
+            onDelete: "cascade"
+        });
+    };
 
     return cb_User;
 };
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const e = require("express");
 
 module.exports = (app) => {
 
@@ -30,11 +31,86 @@ module.exports = (app) => {
     });
 
     // Will Save user and return newly inserted user to frontend
+    app.post("/api/user", (req, res) => {
+
+        const newUser = {
+            userName: req.body.userName,
+            userEmail: req.body.userEmail,
+            userNumber: req.body.userNumber,
+            postTitle: req.body.postTitle,
+            postBody: req.body.postBody,
+            // fk_criteria: req.body.criteria,
+            // fk_instrument: req.body.instrument,
+            // fk_user: req.body.id
+
+        };
+
+        db.cb_User.create(newUser).then((dbUser) => {
+            res.json(dbUser);
+
+        });
+    });
+
+
     app.post("/api/post", (req, res) => {
 
-        db.cb_User.create(req.body).then((dbUser) => {
-            res.json(dbUser);
+        let newUser = {
+            userName: req.body.userName,
+            userEmail: req.body.userEmail,
+            userNumber: req.body.userNumber
+        }
+        let newPost = {
+            postTitle: req.body.postTitle,
+            postBody: req.body.postBody,
+            // fk_user: -1
+            // fk_criteria: req.body.criteria,
+            // fk_instrument: req.body.instrument,
+        };
+
+        let userIdPlaceholder;
+        let newUserId;
+        // Find ID in database
+        db.cb_User.findOne({ where: { userEmail: req.body.userEmail } }).then(user => {
+            if (!user) {
+
+                // Create New User
+                db.cb_User.create(newUser).then((dbUser) => {
+                    newUserId = dbUser.dataValues.id
+                    console.log(newUserId);
+                });
+
+                db.cb_Post.create({
+                    postTitle: req.body.postTitle,
+                    postBody: req.body.postBody,
+                    fk_user: newUserId
+                }).then((dbPost) => {
+                    res.json(dbPost);
+                });
+
+
+            } else {
+                userIdPlaceholder = user.dataValues.id;
+                // CALL UPDATE
+                db.cb_Post.create({
+                    postTitle: req.body.postTitle,
+                    postBody: req.body.postBody,
+                    fk_user: userIdPlaceholder
+                }).then((dbPost) => {
+                    res.json(dbPost);
+                });
+            }
         });
+
+        // db.cb_Post.create({
+        //     userName: req.body.userName,
+        //     userEmail: req.body.userEmail,
+        //     userNumber: req.body.userNumber,
+        //     postTitle: req.body.postTitle,
+        //     postBody: req.body.postBody,
+        //     fk_user: userIdPlaceholder
+        // }).then((dbPost) => {
+        //     res.json(dbPost);
+        // });
     });
 
     // app.get("/api/posts/:user", (req, res) => {
