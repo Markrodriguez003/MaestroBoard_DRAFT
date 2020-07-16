f0817fca34a52f2a9fe00c61fd93aa99e8277f7b
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-07-15 21:59:53 -0400

    made ajax call for instruments

--- public/index.js
+++ public/index.js
@@ -1,71 +1,36 @@
-$(document).ready(function(){
- $("#musicOpt").hide();
- $("#musicGenre").hide();
- $("#bandMate").hide();
-    $('input[type=radio]').click(function(){
-        if (this.id == "exampleRadios1" || "exampleRadios2") {
-            $("#musicGenre").hide();
-            $("#bandMate").hide();
-            $("#musicOpt").show();
-            const instr = [
-                { instrument: "Guitar" },
-                { instrument: "Bass" },
-                { instrument: "Drums" },
-                { instrument: "Percussion" },
-                { instrument: "Brass" },
-                { instrument: "Woodwind" },
-                { instrument: "Synthesizer" },
-                { instrument: "Studio-Equipment" }
-            ];
-        
-            for( let i = 0; i < instr.length; i ++){
-                $("#exampleFormControlSelect1").append(new Option(instr[i].instrument, i))
-                // Repalce "i" with "instruments[i].whatever field is"
-            }
-        
-        
-        
-        
-        
-        
-        //     $.get( "/api/instruments", function()  {
-        // //console.log(instruments)
-        // for( let i = 0; i < instr.length; i ++){
-        //     $("#exampleFormControlSelect1").append(new Option(instr[i], i))
-        //     // Repalce "i" with "instruments[i].whatever field is"
-        // }
-        
-        // });
-        if (this.id == "exampleRadios3"){
-            $("#musicOpt").hide();
-            $("#musicGenre").show();
-            $("#bandMate").show();
-        }
-        }
-       
+$(document).ready(function () {
+  let instr = [];
+
+  // send an AJAX POST-request with jQuery
+  $.get("/api/instruments")
+    // on success, run this callback
+    .then(function (data) {
+      // log the data we found
+      console.log(data);
+      instr = data;
     });
 
-//     const instr = [
-//         { instrument: "Guitar" },
-//         { instrument: "Bass" },
-//         { instrument: "Drums" },
-//         { instrument: "Percussion" },
-//         { instrument: "Brass" },
-//         { instrument: "Woodwind" },
-//         { instrument: "Synthesizer" },
-//         { instrument: "Studio-Equipment" }
-//     ];
-// $.post( "/api/instruments", function()  {
-// //console.log(instruments)
-// for( let i = 0; i < instr.length; i ++){
-//     $("#exampleFormControlSelect1").append(new Option(instr[i], i))
-//     // Repalce "i" with "instruments[i].whatever field is"
-// }
+  $("#musicOpt").hide();
+  $("#musicGenre").hide();
+  $("#bandMate").hide();
+  $("input[type=radio]").click(function () {
+    if (this.id == "exampleRadios1" || "exampleRadios2") {
+      $("#musicGenre").hide();
+      $("#bandMate").hide();
+      $("#musicOpt").show();
 
-// });
-// }
+      for (let i = 0; i < instr.length; i++) {
+        $("#exampleFormControlSelect1").append(
+          new Option(instr[i].instrument, i)
+        );
+        // Repalce "i" with "instruments[i].whatever field is"
+      }
 
+      if (this.id == "exampleRadios3") {
+        $("#musicOpt").hide();
+        $("#musicGenre").show();
+        $("#bandMate").show();
+      }
+    }
+  });
 });
-
-
-
